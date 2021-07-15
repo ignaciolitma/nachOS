0c76868e549a4a68cc00dbd8cc362ca69fb1ba97
ILitman <ignaciolitmanovich@gmail.com>
2021-07-15 18:22:32 -0300

    Update los putss, no pueden usar exit, deben usar return
    Los putss no pueden hacer uso del exit, solo puede hacer uso del return

--- code/userland/cat.c
+++ code/userland/cat.c
@@ -4,19 +4,19 @@ int main(int argc, char** argv) {
 
     if(argc != 2){
         putss("Invalid number of arguments");
-        Exit(-1);
+        return -1;
     }
 
     OpenFileId f = Open(argv[1]);
 
     if(f < 2) {
         putss("Error when openning file");
-        Exit(-1);
+        return -1;
     }
 
     char buff[1];
     for(; Read(buff, 1, f); Write(buff, 1, CONSOLE_OUTPUT));
 
     Close(f);
-    Exit(0);
+    return 0;
 }
